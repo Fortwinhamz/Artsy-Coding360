94ade5fc4a27217ddd80da4e1dc40ad40d7df522
Fortwin <basirathamz@gmail.com>
2023-03-03 18:01:55 +0100

    Made the marketplace responsive

--- script.js
+++ script.js
@@ -2,29 +2,221 @@
 
 
 const menuBtn = document.querySelector(".menu-icon span");
-         const searchBtn = document.querySelector(".search-icon");
-         const cancelBtn = document.querySelector(".cancel-icon");
-         const items = document.querySelector(".nav-items");
-         const form = document.querySelector("form");
-         menuBtn.onclick = ()=>{
-            items.classList.add("active");
-            menuBtn.classList.add("hide");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
-        }
-        cancelBtn.onclick = ()=>{
-            items.classList.remove("active");
-            menuBtn.classList.remove("hide");
-            searchBtn.classList.remove("hide");
-            cancelBtn.classList.remove("show");
-            form.classList.remove("active");
-            cancelBtn.style.color = "#ff3d00";
-        }
-        searchBtn.onclick = ()=>{
-            form.classList.add("active");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+menuBtn.onclick = ()=>{
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
 }
+cancelBtn.onclick = ()=>{
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    form.classList.remove("active");
+    cancelBtn.style.color = "#ff3d00";
+}
+searchBtn.onclick = ()=>{
+    form.classList.add("active");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+const arrow = document.querySelector('.fa-angle-up');
+const arrowCtn = document.querySelector('.artist-bottom')
+
+// arrow.onclick = ()=>{
+//     arrowCtn.classList.remove("hide");
+// }
+
+
+// Category Function toggle code
+
+function catFunction() {
+    var x = document.getElementById("category-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display ="none";
+    //   element.classList.remove("fa-angle-up");
+    } else {
+      x.style.display = "none"; 
+      z.style.display ="block";
+      y.style.display ="none";
+    }
+
+    
+}
+
+function removeCatFunction() {
+    var x = document.getElementById("category-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "block") {
+      z.style.display = "none";
+      x.style.display ="block";
+      element.classList.remove(".fa-angle-up");
+    } else {
+      x.style.display = "block"; 
+      z.style.display ="block";
+    }
+    
+}
+
+
+
+function priceFunction() {
+    var x = document.getElementById("price-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display ="none";
+    //   element.classList.remove("fa-angle-up");
+    } else {
+      x.style.display = "none"; 
+      z.style.display ="block";
+      y.style.display ="none";
+    }
+
+    
+}
+
+function removePriceFunction() {
+    var x = document.getElementById("price-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "block") {
+      z.style.display = "none";
+      x.style.display ="block";
+      element.classList.remove(".fa-angle-up");
+    } else {
+      x.style.display = "block"; 
+      z.style.display ="block";
+    }
+    
+}
+
+
+
+function artFunction() {
+    var x = document.getElementById("artist-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display ="none";
+    //   element.classList.remove("fa-angle-up");
+    } else {
+      x.style.display = "none"; 
+      z.style.display ="block";
+      y.style.display ="none";
+    }
+
+    
+}
+
+function removeArtFunction() {
+    var x = document.getElementById("artist-bottom");
+    var y = document.querySelector('.fa-angle-up')
+    var z = document.querySelector('.fa-angle-down')
+    if (x.style.display === "block") {
+      z.style.display = "none";
+      x.style.display ="block";
+      element.classList.remove(".fa-angle-up");
+    } else {
+      x.style.display = "block"; 
+      z.style.display ="block";
+    }
+    
+}
+
+
+
+// function colFunction() {
+//     var x = document.getElementById("collection-bottom");
+//     var y = document.querySelector('.fa-angle-up')
+//     var z = document.querySelector('.fa-angle-down')
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//       y.style.display ="none";
+//     //   element.classList.remove("fa-angle-up");
+//     } else {
+//       x.style.display = "none"; 
+//       z.style.display ="block";
+//       y.style.display ="none";
+//     }
+
+    
+// }
+
+// function removeColFunction() {
+//     var x = document.getElementById("collection-bottom");
+//     var y = document.querySelector('.fa-angle-up')
+//     var z = document.querySelector('.fa-angle-down')
+//     if (x.style.display === "block") {
+//       z.style.display = "none";
+//       x.style.display ="block";
+//       element.classList.remove(".fa-angle-up");
+//     } else {
+//       x.style.display = "block"; 
+//       z.style.display ="block";
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://twitter.com/notifications';
+const site = 'pagedart.com';
+
+    function submitted(event) {
+    event.preventDefault();
+    const url = google + site + '+' + q.value;
+    const win = window.open(url, '_blank');
+    win.focus();
+    }
+
+    f.addEventListener('submit', submitted);
 
 
 
@@ -48,13 +240,13 @@ const menuBtn = document.querySelector(".menu-icon span");
 
 
 gsap.to(".logo", { 
-    duration: 2,
+    duration: 6,
     // x: 700,
     rotation: 360,
     // repeat: 2, // the number of repeats - this will play 3 times
     yoyo: true,
     repeatDelay: 1,
-    ease: 'bounce.out',
+    ease: 'stagger',
     
 });
 
